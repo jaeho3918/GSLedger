5a5c34dda47f2518ec5aa27acb4430f188b0eaeb
jaeho3918 <46740030+babbybear@users.noreply.github.com>
2020-02-29 19:53:51 +0900

    18

--- app/src/main/java/com/gsgana/gsledger/IntroActivity.kt
+++ app/src/main/java/com/gsgana/gsledger/IntroActivity.kt
@@ -20,11 +20,21 @@ import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 import com.gsgana.gsledger.databinding.ActivityIntroBinding
 import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 class IntroActivity : AppCompatActivity() {
     private val RC_SIGN_IN = 9001
     private val LOG_IN = 18
     private val SIGN_UP = 6
+    private val PREF_NAME = "01504f779d6c77df04"
+    private val ENCRYPT_NAME = "a345f2f713ie8bd261"
+    private val UID_NAME = "7e19f667a8a1c7075f"
+    private val DATABASE_PATH = "qnI4vK2zSUq6GdeT6b"
+    private lateinit var sf: SharedPreferences
+
     private lateinit var mAuth: FirebaseAuth
     private lateinit var googleSigninClient: GoogleSignInClient
     private lateinit var binding: ActivityIntroBinding
@@ -35,18 +45,21 @@ class IntroActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro);
 
-        val sf = this.getSharedPreferences("4vK6O2zSUqGd6", Context.MODE_PRIVATE)
-        val stat = sf?.getInt("ptQY6xnS6Nv6PM", 6)
+        sf = this.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        if (sf.getString(ENCRYPT_NAME, null).isNullOrEmpty()) {
+            //First Signup
+            binding.introProgressBar.visibility = View.GONE
+            googleSignInOption(SIGN_UP, binding)
 
+        } else {
+            //login
+
+        }
         // select signup or login by getSharedPreferences
 
-        // sign up
-        if (stat == SIGN_UP) {
-            binding.introProgressBar.visibility = View.GONE
-            googleSignInOption(stat, binding)
+        val stat = 6
 
-            // login
-        } else if (stat == LOG_IN) {
+        if (stat == LOG_IN) {
             val intent = Intent(applicationContext, MainActivity::class.java)
 //            intent.putExtra(
 //                "key",
@@ -62,7 +75,7 @@ class IntroActivity : AppCompatActivity() {
                 binding.signupBtn.visibility = View.GONE
                 val user = mAuth.currentUser?.uid.toString()
                 val db = FirebaseFirestore.getInstance()
-                val docRef = db.collection("qnI4vKO2zSUq6GdeyT6b").document(user)
+                val docRef = db.collection(DATABASE_PATH).document(user)
                 docRef.get()
                     .addOnSuccessListener { document ->
                         if (document.exists()) {
@@ -88,8 +101,7 @@ class IntroActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        Toast.makeText(this, "11111111111111111111111111", Toast.LENGTH_LONG)
-        val sf = this.getSharedPreferences("4vK6O2zSUqGd6", Context.MODE_PRIVATE) //option
+        val sf = this.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE) //option
         // google login
         if (requestCode === RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
@@ -107,37 +119,53 @@ class IntroActivity : AppCompatActivity() {
         mAuth.signInWithCredential(credential)
             .addOnCompleteListener(this) {
                 if (it.isSuccessful) {
-                    Toast.makeText(this, sf.getString("II4vKO2zSUqGd", null), Toast.LENGTH_LONG)
-
 //                    // Write a message to the firestore
-                    if (sf.getString("II4vKO2zSUqGd", null).isNullOrBlank()) {
-                        sf.edit().putInt("ptQY6xnS6Nv6PM", 18).commit() //save at sharedpreference
-                        sf.edit().putString("II4vKO2zSUqGd", mAuth.currentUser?.uid).commit() //save at sharedpreference
-                        Toast.makeText(this, sf.getString("II4vKO2zSUqGd", null), Toast.LENGTH_LONG)
+                    if (sf.getString(ENCRYPT_NAME, null).isNullOrBlank()) {
+                        // signup and generate key
+                        val test1 = generateRgl()
+                        sf.edit().putString(ENCRYPT_NAME, test1)
+                            .commit()
+                        val test2 = sf.getString(ENCRYPT_NAME, null)
+
+                        sf.edit().putString(UID_NAME, encrypt(this, mAuth.currentUser?.uid!!))
+                            .commit()
+
+                        val test3 = encrypt(this, mAuth.currentUser?.uid!!)
+
+                        val test = decrypt(sf.getString(UID_NAME, null))
 
                         val docRef = FirebaseFirestore.getInstance()
-                            .collection("qnI4vKO2zSUq6GdeyT6b")
-                            .document(sf.getString("II4vKO2zSUqGd", null) ?: "")
-                            .collection(mAuth.currentUser?.uid ?: "")
+                            .collection(DATABASE_PATH)
+                            .document(mAuth.currentUser?.uid!!)
+                            .set(
+                                hashMapOf(
+                                    "Rgl" to generateRgl(),
+                                    "Col0" to 0,
+                                    "Col1" to 0,
+                                    "Col2" to 0,
+                                    "Col3" to 0,
+                                    "Col4" to 0,
+                                    "Col5" to getBytes(this).toString()
+                                )
+                            )
 
                         startActivity(intent)
                         finish()
 
 
-                        Toast.makeText(this, sf.getString("II4vKO2zSUqGd", null), Toast.LENGTH_LONG)
+                        Toast.makeText(this, sf.getString(ENCRYPT_NAME, null), Toast.LENGTH_LONG)
                             .show()
                     } else {
                         Toast.makeText(
                             this,
-                            "already" + sf.getString("II4vKO2zSUqGd", null),
+                            "already" + sf.getString(ENCRYPT_NAME, null),
                             Toast.LENGTH_LONG
                         ).show()
                     }
 
                     mAuth = FirebaseAuth.getInstance()
-                    val user = mAuth.currentUser?.uid.toString()
                     val db = FirebaseFirestore.getInstance()
-                    val docRef = db.collection("qnI4vKO2zSUq6GdeyT6b").document(user)
+                    val docRef = db.collection(DATABASE_PATH).document(mAuth.currentUser?.uid!!)
                     docRef.get()
                         .addOnSuccessListener { document ->
                             if (document.exists()) {
@@ -150,7 +178,7 @@ class IntroActivity : AppCompatActivity() {
                                 finish()
                             } else {
                                 db.collection("users")
-                                    .document(user)
+                                    .document(mAuth.currentUser?.uid!!)
                                     .set(
                                         hashMapOf(
                                             "Rgl" to generateRgl(),
@@ -211,5 +239,49 @@ class IntroActivity : AppCompatActivity() {
         for (i in 0 until 18)
             sb.append(ALLOWED_CHARACTERS[random.nextInt(ALLOWED_CHARACTERS.length)])
         return sb.toString()
+    }
+
+    private fun getBytes(context: Context): ByteArray? {
+        val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val str = prefs.getString(ENCRYPT_NAME, null)
+        return if (!str.isNullOrEmpty()) str.toByteArray(Charsets.ISO_8859_1)
+        else
+            null
+    }
+
+    private fun setBytes(context: Context, byteArray: ByteArray) {
+        val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val e = prefs.edit()
+        e.putString(ENCRYPT_NAME, String(byteArray, Charsets.ISO_8859_1))
+            .commit()
+    }
+
+    private fun encrypt(context: Context, plainText: String): String? = try {
+        // 각각 알고리즘/Block Cipher Mode/Padding 메카니즘이다.
+        sf = this.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING")
+        cipher.init(
+            Cipher.ENCRYPT_MODE,
+            SecretKeySpec(sf.getString(ENCRYPT_NAME, null)?.toByteArray(), "AES")
+        )
+        cipher.doFinal(plainText.toByteArray()).toString()
+
+
+    } catch (e: Exception) {
+        println("Error while encrypting: $e")
+        null
+    }
+
+    private fun decrypt(cipherText: String?): String? = try {
+        // 각각 알고리즘/Block Cipher Mode/Padding 메카니즘이다.
+        val cipher = Cipher.getInstance("AES/ECB/PKC55PADDING")
+        cipher.init(
+            Cipher.DECRYPT_MODE,
+            SecretKeySpec(sf.getString(ENCRYPT_NAME, null)?.toByteArray(), "AES")
+        )
+        String(cipher.doFinal(cipherText?.toByteArray()))
+    } catch (e: Exception) {
+        println("Error while decrypting: $e")
+        null
     }
 }
